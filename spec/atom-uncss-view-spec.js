300c95bcc219af4709fe44e21ffa0ce13219de84
bomsy <b4bomsy@gmail.com>
2016-06-02 11:54:46 +0100

    initial commit

---
+++ spec/atom-uncss-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomUncssView from '../lib/atom-uncss-view';
+
+describe('AtomUncssView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
